aa47634854bcc4f5de443a463d4facf4d2b392ed
Sebastian Mežnar <smeznar@gmail.com>
2024-02-22 14:48:38 +0100

    Podatki in skripte za prve vaje

--- PomozneSkripte/vaje1.py
+++ PomozneSkripte/vaje1.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 
 
-def height_weight_dataset():
+def height_weight_dataset(path):
     male_heights = np.random.normal(177, 7.11, 700) / 100
     woman_heights = np.random.normal(162.57, 6, 300) / 100
     heights = np.concatenate((male_heights, woman_heights))
@@ -16,14 +16,36 @@ def height_weight_dataset():
         s = np.sum(bmi_percentage < np.random.random())
         weight.append((np.random.random()*bmi_scale[s][0]+bmi_scale[s][1])*heights[i]**2)
     weights = np.array(weight)
-    # print(np.mean(weights))
-    gender = ["Male" for i in range(700)] + ["Female" for i in range(300)]
-    df = pd.DataFrame(data={"Height": heights, "Weight": weights, "Gender": gender})
-    df.to_csv("../Podatki/vaje1.csv", index=False)
+    sex = ["Male" for i in range(700)] + ["Female" for i in range(300)]
+    df = pd.DataFrame(data={"Height": heights, "Weight": weights, "Sex": sex})
+    df.to_csv(path, index=False)
+
+
+def weight_dataset(path):
+    height = np.random.normal(170, 6.5, 1000) / 100
+    bmi = np.random.normal(23.5, 2, 1000)
+    weight = bmi * height**2
+    df = pd.DataFrame(data={"Height": height, "Weight": weight})
+    df.to_csv(path, index=False)
 
 
 if __name__ == '__main__':
-    height_weight_dataset()
-    a = pd.read_csv("../Podatki/vaje1.csv")
-    sns.scatterplot(a, y="Height", x="Weight", hue="Gender")
+    height_weight_dataset("../Podatki/vaje1_train.csv")
+    a = pd.read_csv("../Podatki/vaje1a_train.csv")
+    sns.scatterplot(a, y="Height", x="Weight", hue="Sex")
+    plt.show()
+      
+    height_weight_dataset("../Podatki/vaje1_test.csv")
+    a = pd.read_csv("../Podatki/vaje1a_test.csv")
+    sns.scatterplot(a, y="Height", x="Weight", hue="Sex")
+    plt.show()
+  
+    weight_dataset("../Podatki/vaje1b_train.csv")
+    a = pd.read_csv("../Podatki/vaje1b_train.csv")
+    sns.scatterplot(a, y="Height", x="Weight")
+    plt.show()
+  
+    weight_dataset("../Podatki/vaje1b_test.csv")
+    a = pd.read_csv("../Podatki/vaje1b_test.csv")
+    sns.scatterplot(a, y="Height", x="Weight")
     plt.show()
